d87ab1eaaf8f4fdd66d60ce1ce3bc6aaff9de9a9
paulocsilvajr <pauluscave@gmail.com>
2017-01-11 12:05:52 -0200

    Criado funções veriticar_arquivo() e gerar_dicionario_programas() em src/gerenciar_programas

--- src/gerenciador_programas.py
+++ src/gerenciador_programas.py
@@ -2,25 +2,63 @@
 
 from collections import OrderedDict
 
-dic_programas = OrderedDict()
 
-dic_programas = OrderedDict()
+def verificar_arquivo(nome_arquivo: str,
+                      comandos_gerenciadores: tuple=('apt', 'umake', 'pip2', 'pip3', 'add-apt-repository', 'sh')):
+    """ Verificador das entradas usados para instalar programas.
+    :param nome_arquivo: nome do arquivo com lista de programas.
+    :param comandos_gerenciadores: Nome dos gerenciadores de pacote, gerenciador de repositório e outros.
+    :return: ValueError: caso encontre algum erro, None: caso arquivo esteja correto. """
+    with open(nome_arquivo) as f:
+        cont_linha = 0
+        cont_linha_cabecalho = 0
+        cont = {'cabecalho': 0, 'conteudo': 0}
+        for linha in f:
+            cont_linha += 1
+            linha = linha.strip()
 
-diretorio = ''
-with open(diretorio + 'programas') as f:
-    for linha in f:
-        # recortando laterais com espaços
-        texto = linha.strip()
-
-        if texto:
-            # se inicia com #
-            if texto.startswith('#'):
-                chave = texto[1:]
-                dic_programas[chave] = []
+            if linha.startswith('#'):
+                cont['cabecalho'] += 1
+                cont_linha_cabecalho = cont_linha
+            elif linha.startswith(comandos_gerenciadores) and linha.endswith(';'):
+                cont['conteudo'] += 1
             else:
-                dic_programas[chave].append(texto)
+                if cont['cabecalho'] != 1 and cont['conteudo'] < 1:
+                    raise ValueError('Conteúdo do arquivo {0} está inválido, linha {1} à {2}'.
+                                     format(nome_arquivo, cont_linha_cabecalho, cont_linha))
+                else:
+                    cont['cabecalho'] = 0
+                    cont['conteudo'] = 0
 
-# ordenando o dicionário pela chave
-dic_programas = OrderedDict(sorted(dic_programas.items()))
 
-print(dic_programas, dic_programas.keys(), sep='\n')
+def gerar_dicionario_programas(nome_arquivo: str):
+    """ Gerador de dicionário de programas baseado no arquivo informado.
+    :param nome_arquivo: Nome do arquivo com lista de programas.
+    :return: Dicionário usado para instalação de programas. """
+    assert isinstance(nome_arquivo, str), 'Parametro nome_arquivo requer uma string.'
+
+    # verificação do arquivo base
+    verificar_arquivo(nome_arquivo)
+
+    dic_programas = OrderedDict()
+
+    with open(nome_arquivo) as f:
+        for linha in f:
+            # recortando laterais com espaços
+            linha = linha.strip()
+
+            if linha:
+                # se inicia com #
+                if linha.startswith('#'):
+                    chave = linha[1:]
+                    dic_programas[chave] = []
+                else:
+                    dic_programas[chave].append(linha)
+
+    # ordenando o dicionário pela chave
+    return OrderedDict(sorted(dic_programas.items()))
+
+if __name__ == '__main__':
+    DIRETORIO = ''
+    DIC_PROGRAMAS = gerar_dicionario_programas(DIRETORIO + 'programas')
+    print(DIC_PROGRAMAS, DIC_PROGRAMAS.keys(), sep='\n')
