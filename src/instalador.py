5b43f25fc81bd5a070f9e74b9bd48a9bd7c74110
paulocsilvajr <pauluscave@gmail.com>
2017-01-11 15:40:51 -0200

    Adicinado conteúdo em src/instalador.py

--- src/instalador.py
+++ src/instalador.py
@@ -2,10 +2,21 @@
 
 from collections import OrderedDict
 from sys import argv
+from src.gerenciador_programas import gerar_dicionario_programas
+from src.interface_grafica import Instalador
 
 
 def capturar_diretorio():
     return argv[0].replace('instalador.py', '')
 
 
-diretorio = capturar_diretorio()
+def main():
+    # diretorio = capturar_diretorio()
+    diretorio = '/home/paulo/pc/python/instalador-programas/src/'
+    dic_programas = gerar_dicionario_programas(diretorio + 'programas')
+    Instalador(dic_programas, diretorio)
+
+
+main()
+
+
