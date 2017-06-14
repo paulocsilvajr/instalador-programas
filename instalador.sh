88b667afe27a8cb2f40fd5eb2de54736f3118061
paulocsilvajr <pauluscave@gmail.com>
2017-06-13 21:30:52 -0300

    Melhorado instalador.sh e outras pequenas alterações

--- instalador.sh
+++ instalador.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
-#Instalação do idle-python3 se necessário.
+# Verificação se o programa foi executado como root
+if [ "$(id -u)" != "0" ]; then
+    echo "Execute esse programa como ROOT"
+    exit 0
+fi
+
+# Instalação do idle-python3 se necessário.
 verif_idle=$(dpkg --get-selections|grep "^idle-python3*"|grep "\binstall");
 if [[ -z $verif_idle ]];
 then
 	echo "Instalando idle-python3...";
-	sudo apt install idle-python3* -y;
+	apt install idle-python3* -y;
 fi
 
 path=$0;
 path="${path%/*}";
 path=$path/src/instalador.py;
 
-#Se parametro1 estiver vazio.
+# Se parametro1 estiver vazio.
 if [ -z $1 ]; then
-	sudo python3 $path;
+	python3 $path;
 else
-	if [ $1 = "-g" ];
-	then
-		#Verificação se o gksu/gksudo está instalado, caso negativo, instala automaticamente.
-		verif_gksu=$(dpkg --get-selections|grep "^gksu"|grep "\binstall");
-		if [[ -z $verif_gksu ]];
-		then
-			echo "Instalando gksu...";
-			sudo apt install gksu -y;
-		fi
-
-		gksudo python3 $path g;
-	else
-		echo "Parâmetro inválido: $1";
-	fi
+    case $1 in
+        "-h") echo "Instalador de programas para distribuições baseadas em Ubuntu"
+              echo "Interface padrão: cli(terminal)"
+              echo "Sintaxe:" $0 "[-h -g]"
+              echo "-h      Ajuda"
+              echo "-g      Interface gráfica"
+              echo
+              echo "Autor: Paulo C. Silva Junior"
+              echo "Github: paulocsilvajr";;
+        "-g")
+            # Não é mais necessário, pois foi obrigado executar o programa como root
+            # Verificação se o gksu/gksudo está instalado, caso negativo, instala automaticamente.
+#    		verif_gksu=$(dpkg --get-selections|grep "^gksu"|grep "\binstall");
+#    		if [[ -z $verif_gksu ]];
+#    		then
+#    			echo "Instalando gksu...";
+#    			apt install gksu -y;
+#    		fi
+#
+#    		gksudo python3 $path g;
+            python3 $path g;;
+        *)
+		    echo "Parâmetro inválido: $1";;
+	esac
 fi
