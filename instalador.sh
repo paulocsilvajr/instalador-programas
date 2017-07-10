f5683191706b7b853de0a191c298a8b55cd5c2a8
paulocsilvajr <pauluscave@gmail.com>
2017-07-10 14:26:27 -0300

    Refatorado o instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 
-# Verificação se o programa foi executado como root
-if [ "$(id -u)" != "0" ]; then
-    echo "Execute esse programa como ROOT"
-    exit 0
-fi
+path=$0
+path="${path%/*}"
+path=$path/src/instalador.py
 
-# Instalação do idle-python3 se necessário.
-verif_idle=$(dpkg --get-selections|grep "^idle-python3*"|grep "\binstall");
-if [[ -z $verif_idle ]];
-then
-	echo "Instalando idle-python3...";
-	apt install idle-python3* -y;
-fi
+is_root(){
+    # Verificação se o programa foi executado como root
+    if [ "$(id -u)" != "0" ]; then
+        echo "Execute esse programa como ROOT"
+        exit 1
+    fi
+}
 
-path=$0;
-path="${path%/*}";
-path=$path/src/instalador.py;
+install_idle_python(){
+    # Instalação do idle-python3 se necessário.
+    verif_idle=$(dpkg --get-selections|grep "^idle-python3*"|grep "\binstall")
+
+    if [[ -z $verif_idle ]]; then
+        echo "Instalando idle-python3..."
+        apt install idle-python3* -y
+    fi
+}
 
 # Se parametro1 estiver vazio.
 if [ -z $1 ]; then
-	python3 $path;
+    is_root
+
+    python3 $path
 else
     case $1 in
-        "-h") echo "Instalador de programas para distribuições baseadas em Ubuntu"
+        "-h"|"--help")
+              echo "Instalador de programas para distribuições baseadas em Ubuntu."
               echo "Interface padrão: cli(terminal)"
-              echo "Sintaxe:" $0 "[-h -g]"
-              echo "-h      Ajuda"
-              echo "-g      Interface gráfica"
+              echo
+              echo "Sintaxe: $0 [-h --help -g]"
+              echo "-h --help      Ajuda"
+              echo "-g             Interface gráfica"
               echo
               echo "Autor: Paulo C. Silva Junior"
-              echo "Github: paulocsilvajr";;
+              echo "Github: paulocsilvajr"
+              ;;
         "-g")
-            # Não é mais necessário, pois foi obrigado executar o programa como root
-            # Verificação se o gksu/gksudo está instalado, caso negativo, instala automaticamente.
-#    		verif_gksu=$(dpkg --get-selections|grep "^gksu"|grep "\binstall");
-#    		if [[ -z $verif_gksu ]];
-#    		then
-#    			echo "Instalando gksu...";
-#    			apt install gksu -y;
-#    		fi
-#
-#    		gksudo python3 $path g;
-            python3 $path g;;
+            is_root
+            
+            install_idle_python
+
+            python3 $path g
+            ;;
         *)
-		    echo "Parâmetro inválido: $1";;
-	esac
+            echo "Parâmetro inválido: $1, -h para ajuda."
+            exit 1
+            ;;
+    esac
 fi
+
