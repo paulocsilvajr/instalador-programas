2c88093a4350064f767f5d5c4e9c1954f9b4ff15
paulocsilvajr <pauluscave@gmail.com>
2017-07-17 10:36:06 -0300

    Adicionado ajuda em install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ nome_programa="instalador-programas"
 
 is_root(){
     if [ "$(id -u)" != "0" ]; then
-        echo "Execute esse programa como ROOT"
+        echo "Execute esse programa como ROOT, -h para ajuda."
         exit 1
     fi
 }
@@ -17,6 +17,19 @@ make_program(){
     # mensagem de confirmação
     mv -vi $nome_programa $destino$nome_programa && chmod 755 /$destino$nome_programa && echo "Reinicie o terminal para utilizar o programa" $nome_programa
 }
+
+help(){
+    echo "Instalador para o programa ser executado diretamente pelo terminal."
+    echo "Necessário um usuário administrador para executar."
+    echo "Sintaxe: $0 [ -h | --help ]"
+    echo "-h --help      Ajuda"
+    exit 0
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ];
+then
+    help
+fi
 
 is_root
 
