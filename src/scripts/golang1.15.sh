c74c85487f72cd4ab04e19e5736c9bbebc73f6a7
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-10-15 15:01:43 -0300

    Refatorado e melhorado script golang1.15.sh. Testado, instalando corretamente GO 1.15

--- src/scripts/golang1.15.sh
+++ src/scripts/golang1.15.sh
@@ -5,13 +5,28 @@ HOME_USUARIO=/home/$USUARIO
 
 GOLANG=go1.15.linux-amd64
 
-wget https://dl.google.com/go/$GOLANG.tar.gz &&
+if [ -d "/usr/local/go" ]; then
+    versao=$(/usr/local/go/bin/go version)
+    echo "Removendo versão anterior do GO[$versao]"
+    rm -rf /usr/local/go
+fi
+
+adicionar_go_em_profile() {
+    if grep "/usr/local/go/bin" $1 > /dev/null; then
+        echo "Arquivo $1 - OK"
+    else
+        echo 'export PATH=$PATH:/usr/local/go/bin' >> $1 &&
+        echo 'export GOPATH=$HOME/go' >> $1 &&
+        echo 'export PATH=$PATH:$GOPATH/bin' >> $1 &&
+        echo "Atualizado PATH e criado variável de ambiente GOPATH"
+    fi
+}
+
+wget -c https://dl.google.com/go/$GOLANG.tar.gz &&
     tar -C /usr/local/ -xzf $GOLANG.tar.gz &&
     mkdir $HOME_USUARIO/go $HOME_USUARIO/go/bin $HOME_USUARIO/go/pkg $HOME_USUARIO/go/src ||
     chown -R $USUARIO $HOME_USUARIO/go &&
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME_USUARIO/.profile &&
-    echo 'export GOPATH=$HOME/go' >> $HOME_USUARIO/.profile &&
-    echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME_USUARIO/.profile &&
+    adicionar_go_em_profile $HOME_USUARIO/.profile &&
     rm -v $GOLANG.tar.gz &&
     echo 'Disponibilizado variável $GOPATH, diretório ~/go e finalizado instalação.' &&
     echo -e "Para usuários do ZSH, execute o comando abaixo para reconhecer o executável go:\ncp -v ~/.profile ~/.zprofile"
