7a095a8b2aa00687599d2502e20863aa59692519
Paulo C. Silva Jr <pauluscave@gmail.com>
2023-09-12 21:17:36 -0300

    Atualizado scripts de Oh my ZSH e Ultimate vimrc

--- src/scripts/oh-my-zsh.sh
+++ src/scripts/oh-my-zsh.sh
@@ -5,11 +5,13 @@ HOME_USUARIO=/home/$USUARIO
 
 apt install -y wget python3-pygments chroma
 
+echo -e "\n\nRESPONDA y(yes) ao ser questionado para trocar o shell\n\n"
+
 su -l $USUARIO -c "sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
     echo -e "\n\nPara listar temas:"
     echo -e "omz theme list\n"
     echo "Para definir um tema(bureau):"
     echo -e "omz theme set bureau\n"
     echo "Para ativar plugins(git asdf python pip colored-man-pages colorize docker docker-compose ufw):"
-    echo -e "omz plugin enable git asdf python pip colored-man-pages colorize docker docker-compose ufw\n"
+    echo -e "omz plugin enable git python pip colored-man-pages colorize docker docker-compose ufw\n"
 
