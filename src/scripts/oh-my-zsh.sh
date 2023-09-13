32866bd87b6cfc496ca594fffdfd5a8ce646c7c1
Paulo C. Silva Jr <pauluscave@gmail.com>
2023-09-12 21:03:52 -0300

    Refatorado script de Oh my ZSH

--- src/scripts/oh-my-zsh.sh
+++ src/scripts/oh-my-zsh.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-USUARIO=$(users|awk ' {print $1} ');
+USUARIO=$(logname);
 HOME_USUARIO=/home/$USUARIO
 
-su -c "sh -c \"\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)\"" $USUARIO &&
-    chown -R $USUARIO $(ls -d $HOME_USUARIO/.* | grep 'zsh');
+apt install -y wget python3-pygments chroma
+
+su -l $USUARIO -c "sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
+    echo -e "\n\nPara listar temas:"
+    echo -e "omz theme list\n"
+    echo "Para definir um tema(bureau):"
+    echo -e "omz theme set bureau\n"
+    echo "Para ativar plugins(git asdf python pip colored-man-pages colorize docker docker-compose ufw):"
+    echo -e "omz plugin enable git asdf python pip colored-man-pages colorize docker docker-compose ufw\n"
+
